772cab53fa2dc6c9c28b28bcdf082b2546137d44
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-08 00:49:27 +0530

    feat: install deps at runtime

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import {exec} from "node:child_process";
 import {promisify} from "node:util";
 
 const {stderr} = await promisify(exec)("npm --loglevel error ci --only=prod");
-
 if (stderr) {
   throw stderr;
 }
-
-const {setFailed} = await import("@actions/core");
-const {run} = await import("./src/bin.js");
-run().catch(setFailed);
+const {run} = await import("./bin.js");
+run();
