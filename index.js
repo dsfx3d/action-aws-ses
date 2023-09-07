a0e63484612b469f0e412d77a82a5826ffa436e3
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-08 01:40:06 +0530

    fix: declare current working directory

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import {exec} from "node:child_process";
 import {promisify} from "node:util";
 
-const {stderr} = await promisify(exec)("npm --loglevel error ci --only=prod");
+const {stderr} = await promisify(exec)("npm --loglevel error ci --only=prod", {
+  cwd: new URL(".", import.meta.url),
+});
 if (stderr) {
   throw stderr;
 }
