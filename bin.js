81ec4a37acc48158ba9c6a023e45a06a894ecfaa
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-08 00:50:11 +0530

    feat: add action bin

--- bin.js
+++ bin.js
@@ -1,50 +1,47 @@
 import {SESClient, SendEmailCommand} from "@aws-sdk/client-ses";
-import {getInput, setFailed} from "@actions/core";
+import {getInput} from "@actions/core";
 
-try {
-  const bcc = getInput("bcc", {required: false});
-  const body = getInput("body", {required: false});
-  const bodyHtml = getInput("body_html", {required: false});
-  const cc = getInput("cc", {required: false});
-  const from = getInput("from", {required: true});
-  const replyTo = getInput("reply_to", {required: false});
-  const subject = getInput("subject", {required: true});
-  const to = getInput("to", {required: true});
+const bcc = getInput("bcc", {required: false});
+const body = getInput("body", {required: false});
+const bodyHtml = getInput("body_html", {required: false});
+const cc = getInput("cc", {required: false});
+const from = getInput("from", {required: true});
+const replyTo = getInput("reply_to", {required: false});
+const subject = getInput("subject", {required: true});
+const to = getInput("to", {required: true});
 
-  const command = new SendEmailCommand({
-    Destination: {
-      ToAddresses: to.split(","),
-      CcAddresses: cc ? cc.split(",") : undefined,
-      BccAddresses: bcc ? bcc.split(",") : undefined,
+const command = new SendEmailCommand({
+  Destination: {
+    ToAddresses: to.split(","),
+    CcAddresses: cc ? cc.split(",") : undefined,
+    BccAddresses: bcc ? bcc.split(",") : undefined,
+  },
+  Message: {
+    Subject: {
+      Data: subject,
+      Charset: "utf8",
     },
-    Message: {
-      Subject: {
-        Data: subject,
+    Body: {
+      Text: {
+        Data: body,
         Charset: "utf8",
       },
-      Body: {
-        Text: {
-          Data: body,
-          Charset: "utf8",
-        },
-        Html: {
-          Data: bodyHtml,
-          Charset: "utf8",
-        },
+      Html: {
+        Data: bodyHtml,
+        Charset: "utf8",
       },
     },
-    Source: from,
-    ReplyToAddresses: replyTo ? replyTo.split(",") : undefined,
-  });
+  },
+  Source: from,
+  ReplyToAddresses: replyTo ? replyTo.split(",") : undefined,
+});
 
-  new SESClient({
+export const run = () => {
+  return new SESClient({
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
     region: process.env.AWS_DEFAULT_REGION,
   }).send(command);
-} catch (error) {
-  console.log(error);
-  setFailed(error);
-}
+};
