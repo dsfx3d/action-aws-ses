7e211a89e4f247042c0b9699ddd559da711b22ff
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-07 19:58:39 +0530

    feat: add inputs for AWS

--- bin.js
+++ bin.js
@@ -1,17 +1,18 @@
 import {SESClient, SendEmailCommand} from "@aws-sdk/client-ses";
 import {getInput} from "@actions/core";
 
-const subject = getInput("subject", {required: true});
-const from = getInput("from", {required: true});
-const to = getInput("to", {required: true});
-const body = getInput("body", {required: false});
-const htmlBody = getInput("html_body", {required: false});
-const cc = getInput("cc", {required: false});
 const bcc = getInput("bcc", {required: false});
+const body = getInput("body", {required: false});
+const bodyHtml = getInput("body_html", {required: false});
+const cc = getInput("cc", {required: false});
+const from = getInput("from", {required: true});
 const replyTo = getInput("reply_to", {required: false});
+const subject = getInput("subject", {required: true});
+const to = getInput("to", {required: true});
+
 const accessKeyId = getInput("aws_access_key_id", {required: true});
-const secretAccessKey = getInput("aws_secret_access_key", {required: true});
 const region = getInput("aws_region", {required: true});
+const secretAccessKey = getInput("aws_secret_access_key", {required: true});
 
 const command = new SendEmailCommand({
   Destination: {
@@ -30,7 +31,7 @@ const command = new SendEmailCommand({
         Charset: "utf8",
       },
       Html: {
-        Data: htmlBody,
+        Data: bodyHtml,
         Charset: "utf8",
       },
     },
